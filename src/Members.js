dbb2e51440f863fc72b6adc60235364975fe000c
yeawonlee <wwon525@naver.com>
2022-01-25 14:28:21 +0900

    Member List 페이지 함수형 컴포넌트로 변경 및 불필요한 파일 정리

--- src/Members.js
+++ src/Members.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./Members.css";
 import Header from "./Header";
 import BackButton from "./BackButton";
@@ -20,51 +20,66 @@ import { CheckBox } from "devextreme-react/check-box";
 
 const allowedPageSizes = [5, 10, "all"];
 
-class Members extends Component {
-  constructor(props) {
-    super(props);
+const Members = () => {
+  /**
+   * useState
+   * - 가장 기본적인 Hook, 함수형 컴포넌트에서 가변적인 상태를 지닐 수 있게 해 줌
+   * - 함수형 컴포넌트에서 상태를 관리해야 한다면, useState 사용
+   */
+  const [showPageSizeSelector, setShowPageSizeSelector] = useState(true);
+  const [showInfo, setShowInfo] = useState(true);
+  const [showNavButtons, setShowNavButtons] = useState(true);
+  const [data, setData] = useState([]);
 
-    this.state = {
-      showPageSizeSelector: true,
-      showInfo: true,
-      showNavButtons: true,
+  const onRowInserting = (e) => {
+    postMember(e);
+  };
 
-      data: [],
-    };
+  const onRowUpdating = (e) => {
+    putMember(e);
+  };
 
-    this.onRowInserting = (e) => {
-      this.postMember(e);
-    };
+  const onRowRemoving = (e) => {
+    deleteMember(e);
+  };
 
-    this.onRowUpdating = (e) => {
-      this.putMember(e);
-    };
-
-    this.onRowRemoving = (e) => {
-      this.deleteMember(e);
-    };
-  }
-
-  componentDidMount() {
-    this.getMembers();
-  }
+  /**
+   * useEffect (p. 194 ~, p. 371, 372)
+   * - 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook
+   * - componentDidMount + componentDidUpdate 형태
+   * - 마운트될 때만 실행하고 싶을 때: useEffect(() => {}, []); // 두 번째 파라미터로 빈 배열
+   * - 기본적으로 렌더링되고 난 직후마다 실행되며, 두 번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라짐
+   * - 컴포넌트가 언마운트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면, useEffect에서 뒷정리(cleanup) 함수를 반환해 줘야 함
+   * - 비동기적으로 동작한다.
+   * - 주의할 점: useEffect에 등록하는 함수에 async 붙이면 X. ∵ useEffect에서 반환해야 하는 값은 뒷정리 함수.
+   * - 
+   */
+  useEffect(() => {
+    getMembers();
+  }, []);
 
   // axios GET / POST / PUT / DELETE
 
+  /**
+   * 비동기(async) : (요청과 응답이 동시 X →) 안 기다림
+   * 동기(sync) : (요청과 응답이 동시에 →) 기다림
+   * async/await : `async` - Promise를 동기적으로 다룰 수 있게 해 줌. 기다리도록 한다.
+   */
+
   // (GET) 전체 회원 조회
-  getMembers = async () => {
+  const getMembers = async () => {
     try {
       const response = await axios.get(
         "https://www.yeawonlee.com/spring-mvc/api/members"
       );
-      this.setState({ data: response.data });
+      setData(response.data);
     } catch (e) {
       console.error(e);
     }
   };
 
   // (POST) 회원 등록
-  postMember = async (e) => {
+  const postMember = async (e) => {
     //onRowInserting 매개변수 data: 삽입해야 하는 행의 데이터
     console.log(`post test data: ${e.data}`); // post test data: [object Object]
 
@@ -77,7 +92,8 @@ class Members extends Component {
 
     try {
       //const response = await axios.post("/spring-mvc/api/members",  // 개발
-      const response = await axios.post("https://www.yeawonlee.com/spring-mvc/api/members",    // 배포
+      const response = await axios.post(
+        "https://www.yeawonlee.com/spring-mvc/api/members", // 배포
         {
           idx: e.data.idx,
           id: e.data.id,
@@ -85,25 +101,18 @@ class Members extends Component {
           name: e.data.name,
           email: e.data.email,
         }
-        /*,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }*/
       );
 
-      this.setState({ data: response.data });
+      setData(response.data);
       console.log(response.data);
-      this.getMembers();
+      getMembers();
     } catch (e) {
-      // 여기서 에러가 나는 듯. Network Error
       console.error(e);
     }
   };
 
   // (PUT) 회원 정보 수정
-  putMember = async (e) => {
+  const putMember = async (e) => {
     //onRowUpdating 매개변수 key: 행의 키
     //onRowUpdating 매개변수 newData: 행의 업데이트된 데이터
     console.log(`put test key: ${e.key}`); // put test key: 27(선택한 키 값)
@@ -111,33 +120,37 @@ class Members extends Component {
 
     try {
       //const response = await axios.put(`/spring-mvc/api/members/${e.key}`, // 개발
-      const response = await axios.put(`https://www.yeawonlee.com/spring-mvc/api/members/${e.key}`,   // 배포
+      const response = await axios.put(
+        `https://www.yeawonlee.com/spring-mvc/api/members/${e.key}`, // 배포
         {
           idx: e.oldData.idx,
-          id: (e.newData.id != null) ? e.newData.id : e.oldData.id,
-          password: (e.newData.password != null) ? e.newData.password : e.oldData.password,
-          name: (e.newData.name != null) ? e.newData.name : e.oldData.name,
-          email: (e.newData.email != null) ? e.newData.email : e.oldData.email
+          id: e.newData.id != null ? e.newData.id : e.oldData.id,
+          password:
+            e.newData.password != null
+              ? e.newData.password
+              : e.oldData.password,
+          name: e.newData.name != null ? e.newData.name : e.oldData.name,
+          email: e.newData.email != null ? e.newData.email : e.oldData.email,
         }
       );
-      this.setState({ data: response.data });
-      console.log(`data(state): ${this.state.data.data}`);
-      this.getMembers();
+      setData(response.data);
+      //console.log(`data(state): ${this.state.data.data}`);
+      getMembers();
     } catch (e) {
       console.error(e);
     }
   };
 
   // (DLETE) 회원 삭제
-  deleteMember = async (e) => {
+  const deleteMember = async (e) => {
     //onRowRemoving의 매개변수 key: 행의 키
     console.log(`delete test key: ${e.key}`); // delete test key: 27(선택한 키 값)
     try {
       const response = await axios.delete(
         `https://www.yeawonlee.com/spring-mvc/api/members/${e.key}`
       );
-      this.setState({ data: response.data });
-      this.getMembers();
+      setData(response.data);
+      getMembers();
     } catch (e) {
       console.error(e);
     }
@@ -147,122 +160,109 @@ class Members extends Component {
 
   // page, nav button ...
 
-  showPageSizeSelectorChange = (value) => {
-    this.setState({
-      ...this.state,
-      showPageSizeSelector: value,
-    });
+  const showPageSizeSelectorChange = (value) => {
+    setShowPageSizeSelector(value);
   };
 
-  showInfoChange = (value) => {
-    this.setState({
-      ...this.state,
-      showInfo: value,
-    });
+  const showInfoChange = (value) => {
+    setShowInfo(value);
   };
 
-  showNavButtonsChange = (value) => {
-    this.setState({
-      ...this.state,
-      showNavButtons: value,
-    });
+  const showNavButtonsChange = (value) => {
+    setShowNavButtons(value);
   };
 
-  customizeColumns(columns) {
+  const customizeColumns = (columns) => {
     columns[0].width = 70;
-  }
+  };
 
-  /////////////////////////////////////////////////////////////////////////////////
+  return (
+    <div className="App">
+      <Header title="Member list"></Header>
+      <main>
+        <section>
+          <div>
+            <div className="options">
+              <div className="caption">Options</div>
+              <div className="option-container">
+                <div className="option">
+                  <CheckBox
+                    id="showPageSizes"
+                    text="Show Page Size Selector"
+                    value={showPageSizeSelector}
+                    onValueChange={showPageSizeSelectorChange}
+                  ></CheckBox>
+                </div>
 
-  render() {
-    return (
-      <div className="App">
-        <Header title="Member list"></Header>
-        <main>
-          <section>
-            <div>
-              <div className="options">
-                <div className="caption">Options</div>
-                <div className="option-container">
-                  <div className="option">
-                    <CheckBox
-                      id="showPageSizes"
-                      text="Show Page Size Selector"
-                      value={this.state.showPageSizeSelector}
-                      onValueChange={this.showPageSizeSelectorChange}
-                    ></CheckBox>
-                  </div>
+                <div className="option">
+                  <CheckBox
+                    id="showInfo"
+                    text="Show Info Text"
+                    value={showInfo}
+                    onValueChange={showInfoChange}
+                  ></CheckBox>
+                </div>
 
-                  <div className="option">
-                    <CheckBox
-                      id="showInfo"
-                      text="Show Info Text"
-                      value={this.state.showInfo}
-                      onValueChange={this.showInfoChange}
-                    ></CheckBox>
-                  </div>
-
-                  <div className="option">
-                    <CheckBox
-                      id="showNavButtons"
-                      text="Show Navigation Buttons"
-                      value={this.state.showNavButtons}
-                      onValueChange={this.showNavButtonsChange}
-                    ></CheckBox>
-                  </div>
+                <div className="option">
+                  <CheckBox
+                    id="showNavButtons"
+                    text="Show Navigation Buttons"
+                    value={showNavButtons}
+                    onValueChange={showNavButtonsChange}
+                  ></CheckBox>
                 </div>
               </div>
-
-              <React.Fragment>
-                <DataGrid
-                  id="gridContainer"
-                  dataSource={this.state.data}
-                  RemoteOperations={true}
-                  keyExpr="idx"
-                  showBorders={true}
-                  hoverStateEnabled={true}
-                  customizeColumns={this.customizeColumns}
-                  onRowInserting={this.onRowInserting}
-                  onRowUpdating={this.onRowUpdating}
-                  onRowRemoving={this.onRowRemoving}
-                >
-                  <Column
-                    dataField="idx"
-                    dataType="number"
-                    caption="No"
-                    width={70}
-                  />
-                  <Column dataField="id" dataType="string" />
-                  <Column dataField="password" dataType="string" />
-                  <Column dataField="name" dataType="string" />
-                  <Column dataField="email" dataType="string" />
-
-                  <Scrolling rowRenderingMode="virtual"></Scrolling>
-                  <Paging> defualtPageSize={10}</Paging>
-
-                  <Pager
-                    visible={true}
-                    allowedPageSizes={allowedPageSizes}
-                    showPageSizeSelector={this.state.showPageSizeSelector}
-                    showInfo={this.state.showInfo}
-                    showNavigationButtons={this.state.showNavButtons}
-                  ></Pager>
-
-                  <Editing
-                    mode="row"
-                    allowAdding={true}
-                    allowUpdating={true}
-                    allowDeleting={true}
-                  ></Editing>
-                </DataGrid>
-              </React.Fragment>
             </div>
-          </section>
-        </main>
-        <BackButton></BackButton>
-      </div>
-    );
-  }
-}
+
+            <React.Fragment>
+              <DataGrid
+                id="gridContainer"
+                dataSource={data}
+                RemoteOperations={true}
+                keyExpr="idx"
+                showBorders={true}
+                hoverStateEnabled={true}
+                customizeColumns={customizeColumns}
+                onRowInserting={onRowInserting}
+                onRowUpdating={onRowUpdating}
+                onRowRemoving={onRowRemoving}
+              >
+                <Column
+                  dataField="idx"
+                  dataType="number"
+                  caption="No"
+                  width={70}
+                />
+                <Column dataField="id" dataType="string" />
+                <Column dataField="password" dataType="string" />
+                <Column dataField="name" dataType="string" />
+                <Column dataField="email" dataType="string" />
+
+                <Scrolling rowRenderingMode="virtual"></Scrolling>
+                <Paging> defualtPageSize={10}</Paging>
+
+                <Pager
+                  visible={true}
+                  allowedPageSizes={allowedPageSizes}
+                  showPageSizeSelector={showPageSizeSelector}
+                  showInfo={showInfo}
+                  showNavigationButtons={showNavButtons}
+                ></Pager>
+
+                <Editing
+                  mode="row"
+                  allowAdding={true}
+                  allowUpdating={true}
+                  allowDeleting={true}
+                ></Editing>
+              </DataGrid>
+            </React.Fragment>
+          </div>
+        </section>
+      </main>
+      <BackButton></BackButton>
+    </div>
+  );
+};
 
 export default Members;
