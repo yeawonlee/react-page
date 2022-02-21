4774936abd40fe9923ff5e3d8155194fdd61716b
yeawonlee <wwon525@naver.com>
2022-02-21 11:28:03 +0900

    NavigationDrawer.js → App.js 수정 및 Router 사용 부분 수정, 테마 변경 기능 구현 중

--- src/App.js
+++ src/App.js
@@ -1,33 +1,313 @@
-import "devextreme/dist/css/dx.common.css";
-import "devextreme/dist/css/dx.light.css";
-import "./App.css";
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { useNavigate } from "react-router-dom";
 
-import NavigationDrawer from "./NavigationDrawer";
+import ChangeTheme from "./ChangeTheme";
+import ChangeThemeButton from "./ChangeThemeButton";
+
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Members from "./Members";
 import Map from "./Map";
 import Footer from "./Footer";
+import "./App.css";
 
-const App = () => {
+const drawerWidth = 240;
+
+const pages = ["Product", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "DashBoard", "Logout"];
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+/* ------------------- Main 함수 ------------------- */
+const App = (props) => {
+  // Drawer
+  const theme = useTheme();
+
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const navigation = useNavigate();
+  const itemList = [
+    {
+      text: "Home",
+      onClick: () => navigation("/"),
+    },
+    {
+      text: "Member Service",
+      onClick: () => navigation("/members"),
+    },
+    {
+      text: "Map Control",
+      onClick: () => navigation("/maps"),
+    },
+    /*
+    {
+      text: "UI Component Test",
+      onClick: () => history.push("/ui-component-test"),
+    },
+    */
+  ];
+
+  // Nested List
+  const [nestedListOpen, setNestedListOpen] = useState(true);
+
+  const handleClick = () => {
+    setNestedListOpen(!nestedListOpen);
+    //console.log(`nestedListOpen : ${nestedListOpen}`);
+  };
+
+  // App Bar
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div className="App">
-      <NavigationDrawer/>
-      <Routes>
-        <Route path="/" element={<Home title="Home Page"/>}></Route>
-        <Route path="/maps" element={<Map />}></Route>
-        <Route path="/members" element={<Members />}></Route>
-      </Routes>
-      <Footer />
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
 
-      {/*
-      <main>
-        <section>
-        </section>
-      </main>
-      */}
-    </div>
+      {/* Drawer Demo Code */}
+      <AppBar position="fixed" open={open}>
+        <Container maxWidth="x1">
+          <Toolbar disableGutters>
+            {/* LOGO 이미지 */}
+            <Box sx={{ mr: 2, display: "flex" }}>
+              <img alt="LOGO" src="img/cloud-computing.png" width={60} />
+            </Box>
+
+            {/* Menu 아이콘 */}
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            {/* 상단 메뉴 (Products, Pricing, Blog) */}
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <Menu
+                id="menu"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            {/* 테마 변경 아이콘  */}
+            <Box
+              sx={{
+                mx: 2,
+                flexGrow: 0,
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              <ChangeThemeButton />
+            </Box>
+
+            {/* 사용자 정보  */}
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+
+        <Divider />
+
+        <List>
+          {itemList.map((item, index) => {
+            const { text, onClick } = item;
+            return (
+              <ListItem button key={text} onClick={onClick}>
+                <ListItemText primary={text} />
+              </ListItem>
+            );
+          })}
+
+          {/* UI Component Test 메뉴 */}
+          {/*<ListItemButton onClick={handleClick}>*/}
+          <ListItem>
+            <ListItemText primary="UI Component Test" onClick={handleClick} />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {["Grid Test", "Pivot Grid Test", "Chart Test"].map((text) => (
+                <ListItem button key={text} sx={{ pl: 4 }}>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </Collapse>
+        </List>
+      </Drawer>
+
+      <Main open={open}>
+        <DrawerHeader />
+        <Routes>
+          <Route path="/" element={<Home title="Home Page" />}></Route>
+          <Route path="/maps" element={<Map />}></Route>
+          <Route path="/members" element={<Members />}></Route>
+        </Routes>
+        <Footer />
+      </Main>
+    </Box>
   );
 };
 
---
+++ src/ChangeTheme.js
@@ -0,0 +1,78 @@
+import React, { useState, useMemo } from "react";
+import {
+  useTheme,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function MyApp() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "background.default",
+        color: "text.primary",
+        borderRadius: 1,
+        p: 3,
+      }}
+    >
+      {theme.palette.mode} mode
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? (
+          <Brightness7Icon />
+        ) : (
+          <Brightness4Icon />
+        )}
+      </IconButton>
+    </Box>
+  );
+}
+
+const ChangeTheme = () => {
+  // Theme
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <MyApp />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default ChangeTheme;
