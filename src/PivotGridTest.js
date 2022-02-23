e190f7280a068cbf6776b5a8c811cff8ecac1d8d
yeawonlee <wwon525@naver.com>
2022-02-23 10:26:04 +0900

    Pivot Grid Test 페이지 작성

---
+++ src/PivotGridTest.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { PivotGrid, Scrolling } from "devextreme-react/pivot-grid";
+import { createStore } from "devextreme-aspnet-data-nojquery";
+
+/**
+ * Pivot Grid > Data Binding > Web API Service
+ * (https://js.devexpress.com/Demos/WidgetsGallery/Demo/PivotGrid/WebAPIService/React/Light/)
+ */
+
+const dataSource = {
+  remoteOperations: true,
+  store: createStore({
+    key: "OrderID",
+    loadUrl:
+      "https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/Sales/Orders",
+  }),
+  fields: [
+    {
+      caption: "Category",
+      dataField: "ProductCategoryName",
+      width: 250,
+      expanded: true,
+      sortBySummaryField: "SalesAmount",
+      sortBySummaryPath: [],
+      sortOrder: "desc",
+      area: "row",
+    },
+    {
+      caption: "Subcategory",
+      dataField: "ProductSubcategoryName",
+      width: 250,
+      sortBySummaryField: "SalesAmount",
+      sortBySummaryPath: [],
+      sortOrder: "desc",
+      area: "row",
+    },
+    {
+      caption: "Product",
+      dataField: "ProductName",
+      area: "row",
+      sortBySummaryField: "SalesAmount",
+      sortBySummaryPath: [],
+      sortOrder: "desc",
+      width: 250,
+    },
+    {
+      caption: "Date",
+      dataField: "DateKey",
+      dataType: "date",
+      area: "column",
+    },
+    {
+      caption: "Amount",
+      dataField: "SalesAmount",
+      summaryType: "sum",
+      format: "currency",
+      area: "data",
+    },
+    {
+      caption: "Store",
+      dataField: "StoreName",
+    },
+    {
+      caption: "Quantity",
+      dataField: "SalesQuantity",
+      summaryType: "sum",
+    },
+    {
+      caption: "Unit Price",
+      dataField: "UnitPrice",
+      format: "currency",
+      summaryType: "sum",
+    },
+    {
+      dataField: "Id",
+      visible: false,
+    },
+  ],
+};
+
+const PivotGridTest = () => {
+  return (
+    <main>
+      <section>
+        <h1>Pivot Grid Test</h1>
+        <PivotGrid
+          allowSorting={true}
+          allowSortingBySummary={true}
+          allowFiltering={true}
+          height={620}
+          showBorders={true}
+          rowHeaderLayout="tree"
+          dataSource={dataSource}
+        >
+          <Scrolling mode="virtual" />
+        </PivotGrid>
+      </section>
+    </main>
+  );
+};
+
+export default PivotGridTest;
